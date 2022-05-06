ada5236de8e85a3ccd7b0bb316d3c508e5836aa4
Hyeonjun <wns8363@gmail.com>
2022-05-07 01:25:36 +0900

    병합 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
     public void PlayAnimation(AttackStyle cmd)
     {
         curAttack = cmd;
-
+        SoundManager.instance.PlayRandomAttackVfx();
         switch(curAttack)
         {
             case AttackStyle.JAP:

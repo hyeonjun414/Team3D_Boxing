4dcc288f0413709cb064620c31f3518f80fe79e2
Hyeonjun <wns8363@gmail.com>
2022-05-07 18:31:12 +0900

    마무리 빌드

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class MoveCamera : MonoBehaviour
+{
+    CinemachineVirtualCamera virtualCamera;
+    private void Awake()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+    public void OnClickSingle()
+    {
+        virtualCamera.Priority = 0;
+        StartCoroutine(LoadMain());
+        
+    }
+    public void OnClickDouble()
+    {
+        virtualCamera.Priority = 0;
+        StartCoroutine(LoadMain());
+    }
+    IEnumerator LoadMain()
+    {
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene("MainScene");
+    }
+}
